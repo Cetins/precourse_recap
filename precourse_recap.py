b78d4b02878a46032a23ca3bba0f5f13483fa8e5
Cetins <69481095+Cetins@users.noreply.github.com>
2022-05-16 22:54:04 +0100

    file modified

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,9 +1,7 @@
 fahrenheit_temperature = int(input("Enter temperature in Fahrenheit: "))
 
-#print(fahrenheit_temperature)
-
 def convert_temperature(temperature):
     temperature = round((fahrenheit_temperature - 32) * 5/9)
-    print("Temperature in Celcious:", temperature)
+    print("Temperature in Celcius:", temperature)
 
 convert_temperature(fahrenheit_temperature)
